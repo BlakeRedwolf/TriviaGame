6b33f57f727781a8739dab7fcc003961ff02cbf1
Blake Nelson <Blake Nelson>
2017-03-08 01:08:17 -0800

    All content & animations added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,38 @@
-function playGame() {
-	$('#startbtn').hide();
-	setTimeout(function(){ 
-		var finalScore = $('#CorrectGuesses').val();
-		alert(finalScore); }, 10000);
+ function playGame() {
+ 	$('#startbtn').html('&nbsp;');
+ 	$('#startbtn').removeClass('btn-success');
+ 	
+ 	var tmr = setInterval(ckgame, 1000); 
+ 		$('#header h4').html('&nbsp;')
+ 		seconds = 15000;
+ 		function ckgame(){
+ 			seconds = seconds-1000;
+ 			s = seconds / 1000;
+ 			// console.log(seconds);
+ 			$("#tremain").text(s);
+ 			if(seconds <=0){
+ 			clearInterval(tmr);
+ 			//alert('Done');
+ 			var finalScore = $('#CorrectGuesses').val();
+ 			//alert(finalScore);
+			$('#FinalModal').css({'display':'block'}); 
+ 			$('#FScore').text(finalScore);
+ 			
+ 			return;
+ 			}
+ 		}
 
-};
+ };
 
 function guess(userguess) {
 	if (userguess == "choose_c1" || userguess == 'choose_b2' || userguess == 'choose_d3' || userguess == 'choose_b4' || userguess == 'choose_d5') {
-		alert("Correct!");
+		//alert("Correct!");
+		$( ".alert-success" ).fadeIn( "fast", function() {
+			// Animation complete
+			$( ".alert-success" ).fadeOut( "slow", function() {
+			// Animation complete
+			});
+		});
 		var score = $('#CorrectGuesses').val();
 		if (!score){NewScore = 1}
 		else{	
@@ -16,7 +40,15 @@ function guess(userguess) {
 		$('#CorrectGuesses').val(Number(NewScore));
 	}
 	}
-	else alert('Wrong Fucker!!!');
+	else{ 
+		//alert('Wrong Fucker!!!');
+			$( ".alert-danger" ).fadeIn( "fast", function() {
+			// Animation complete
+			$( ".alert-danger" ).fadeOut( "slow", function() {
+			// Animation complete
+			});
+		});
+	}	
 };
 
 $( document ).ready(function() {
